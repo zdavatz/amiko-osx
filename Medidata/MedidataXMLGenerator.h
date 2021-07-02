de070f081b4253ad0a16e8b90f02ecadd9039aa1
b123400 <i@b123400.net>
2021-07-02 21:12:26 +0900

    First step to generate xml for Medidata

---
+++ Medidata/MedidataXMLGenerator.h
@@ -0,0 +1,20 @@
+//
+//  MedidataXMLGenerator.h
+//  AmiKo
+//
+//  Created by b123400 on 2021/07/02.
+//  Copyright © 2021 Ywesee GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MLOperator.h"
+#import "MLPatient.h"
+#import "MLPrescriptionItem.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MedidataXMLGenerator : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
