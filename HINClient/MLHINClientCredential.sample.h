8eebab4ab307b8b3b38745efc2d94b20dc429088
b123400 <i@b123400.net>
2023-06-28 22:22:45 +0900

    Implement client for HIN

---
+++ HINClient/MLHINClientCredential.sample.h
@@ -0,0 +1,15 @@
+//
+//  MLHINClientCredential.h
+//  AmiKo
+//
+//  Created by b123400 on 2023/06/28.
+//  Copyright © 2023 Ywesee GmbH. All rights reserved.
+//
+
+#ifndef MLHINClientCredential_h
+#define MLHINClientCredential_h
+
+#define HIN_CLIENT_ID @"xxxxxxxx"
+#define HIN_CLIENT_SECRET @"xxxxxxxxxx"
+
+#endif /* MLHINClientCredential_h */
